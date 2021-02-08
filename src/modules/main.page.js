d86405d021ae9c37d6233fd9d8de15bbc7a547e0
unknown <jn16h7@gmail.com>
2021-02-08 23:32:47 +0800

    fix privacy

--- src/modules/main.page.js
+++ src/modules/main.page.js
@@ -9,12 +9,15 @@ import { Services } from './components/Services';
 import { News } from './components/News'; 
 import Footer from './components/Footer/Footer'
 import QierPlayer from 'qier-player';
-import company from './videos/Company.mp4';
-import escalators from './videos/Escalators.mp4';
-import passenger from './videos/Passenger_Elevators.mp4';
+// import company from './videos/Company.mp4';
+// import escalators from './videos/Escalators.mp4';
+// import passenger from './videos/Passenger_Elevators.mp4';
 import { ContactUs } from './components/Contactus';
 
 function MainPage() {
+  const company = "https://firebasestorage.googleapis.com/v0/b/volkslift-ph.appspot.com/o/Company.mp4?alt=media&token=d231ba04-3d69-417b-b7da-0a09e9f424c3";
+  const escalators = "https://firebasestorage.googleapis.com/v0/b/volkslift-ph.appspot.com/o/Escalators.mp4?alt=media&token=c08d1837-d516-4eb5-be60-b322bfe3e9d1";
+  const passenger = "https://firebasestorage.googleapis.com/v0/b/volkslift-ph.appspot.com/o/Passenger_Elevators.mp4?alt=media&token=6a86c0a4-8608-4157-90bf-8c7732e0f13f";
   return (
     <div className="App">
       {/* start nav */}
